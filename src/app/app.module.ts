400dc5f7e5fdebd951d3985c7c77f9f47d079697
VaniaPatzi <100331206+VaniaPatzi@users.noreply.github.com>
2023-11-16 14:24:27 +0000

    generando el componente menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { HttpClientModule } from '@angular/common/http';
 import { TareasComponent } from './components/tareas/tareas.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     InicioComponent,
-    TareasComponent
+    TareasComponent,
+    EncabezadoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
